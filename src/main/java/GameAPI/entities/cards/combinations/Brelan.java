9b1340bd6c6315d5caa5355b8fe3abe8c4f3bbfa
Louis-Saglio <louis.saglio@sfr.fr>
2018-11-19 17:14:51 +0100

    End brelan implementation

--- src/main/java/GameAPI/entities/cards/combinations/Brelan.java
+++ src/main/java/GameAPI/entities/cards/combinations/Brelan.java
@@ -8,18 +8,37 @@ import GameAPI.entities.cards.combinations.exceptions.CombinationNotPresentExcep
 import java.util.Comparator;
 
 public class Brelan extends Combination {
-  private static final Integer value = DoublePaire.getNextValue();
 
-  Brelan(Cards cards) {
+  private static final Integer value = DoublePaire.getNextValue();
+  private final Rank rank;
+
+  static Integer getNextValue() {
+    return value + 1;
+  }
+
+  Brelan(Rank rank) {
     super(value);
-    cards.getRanksByMinimumNbr(3)
+    this.rank = rank;
+  }
+
+  public Brelan(Cards cards) {
+    super(value);
+    rank = cards.getRanksByMinimumNbr(3)
         .stream()
         .max(Comparator.comparingInt(Rank::getValue))
         .orElseThrow(() -> new CombinationNotPresentException("Error while creating brelan"));
   }
 
   @Override
-  Integer comparesWithSame(Combination combination) {
-    return null;
+  protected Integer comparesWithSame(Combination combination) {
+    Brelan brelan = (Brelan) combination;
+    if (this.rank.getValue() > brelan.rank.getValue()) return 1;
+    else if (this.rank.getValue() < brelan.rank.getValue()) return -1;
+    return 0;
+  }
+
+  @Override
+  public String toString() {
+    return "Brelan of " + rank;
   }
 }
